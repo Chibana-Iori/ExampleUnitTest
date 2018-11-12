b6e4db514d18cff4bab6346a531759ce40172300
Iori Chibana <e185746@ie.u-ryukyu.ac.jp>
2018-11-13 08:09:31 +0900

    初期コード

---
+++ src/test/jp/ac/uryukyu/ie/tnal/EnemyTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.tnal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+    }
+}
