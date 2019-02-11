16b5d8f782f09704d8a60240459c8658a22a5dee
Iori Chibana <e185746@ie.u-ryukyu.ac.jp>
2019-02-11 14:24:58 +0900

    second

--- src/main/jp/ac/uryukyu/ie/tnal/Main.java
+++ src/main/jp/ac/uryukyu/ie/tnal/Main.java
@@ -5,8 +5,8 @@ package jp.ac.uryukyu.ie.tnal;
  */
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
+        Hero hero = new Hero();
+        Enemy enemy = new Enemy();
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
